a176d57b84f470d849cf868b286be8d89f20c91f
andrii <quadr988@gmail.com>
2022-02-11 21:00:23 +0200

    frontend, build

---
+++ src/main/resources/js/src/frontend.js
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from "../_snowpack/pkg/react.js";
+function Frontend() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    console.log("test");
+    setData([1, 2, 3]);
+  }, []);
+  return /* @__PURE__ */ React.createElement("div", null, "TEST");
+}
+export default Frontend;
