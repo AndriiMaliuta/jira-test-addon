1d0cd533105f1a23c10d93f36afc97e5c6539eed
Andrii Maliuta <quadr988@gmail.com>
2022-02-13 15:09:05 +0200

    frontend as SP

--- src/main/java/com/anma/atl/impl/menus/CustomScreenOne.java
+++ src/main/java/com/anma/atl/impl/menus/CustomScreenOne.java
@@ -2,7 +2,9 @@ package com.anma.atl.impl.menus;
 
 import com.anma.atl.impl.srv.EventsService;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.atlassian.plugin.spring.scanner.annotation.imports.JiraImport;
 import com.atlassian.templaterenderer.TemplateRenderer;
+import com.atlassian.webresource.api.assembler.PageBuilderService;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
@@ -18,18 +20,23 @@ public class CustomScreenOne extends HttpServlet {
 
     private final TemplateRenderer templateRenderer;
     private final EventsService eventsService;
+    private PageBuilderService pageBuilderService;
 
     @Inject
     public CustomScreenOne(@ComponentImport TemplateRenderer templateRenderer,
-                           EventsService eventsService) {
+                           EventsService eventsService,
+                           @JiraImport PageBuilderService pageBuilderService) {
         this.templateRenderer = templateRenderer;
         this.eventsService = eventsService;
+        this.pageBuilderService = pageBuilderService;
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         Map<String, Object> context = new HashMap<>();
+
+//        pageBuilderService.assembler().resources().requireWebResource("com.anma.atl.jira-test:require-test-resource");
 
         context.put("contextPath", req.getContextPath());
         context.put("records", eventsService.getAll());
